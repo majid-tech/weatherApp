3f792d80738061cbf15c57028bc2407973cc5476
majid-tech <majidmohammed044@gmail.com>
2025-09-22 10:18:17 +0000

    created 3 files

---
+++ app.js
@@ -0,0 +1,16 @@
+// const img = document.querySelector('img');
+// const searchbtn = document.querySelector('#search-button');
+
+// searchbtn.addEventListener("click", ()=>{
+//     let searchinput = document.querySelector('#search').value;
+
+//     fetch(`https://api.giphy.com/v1/gifs/translate?api_key=JV9CoVu8Ho9JY70sChP1oVqTM09Z2AJF&s=${searchinput}`)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(data) {
+//       img.src = data.data.images.original.url;
+//     });
+// });
+
+  
