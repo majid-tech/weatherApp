60bd36dcd51adb01d90d969f55bd134b5b1fe65e
majid-tech <majidmohammed044@gmail.com>
2025-09-22 11:42:09 +0000

    commented the giph codes

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 // const img = document.querySelector('img');
 // const searchbtn = document.querySelector('#search-button');
+// let searchinput = document.querySelector('#search').value;
 
-// searchbtn.addEventListener("click", ()=>{
-//     let searchinput = document.querySelector('#search').value;
+// // searchbtn.addEventListener("click", ()=>{
+// //     let searchinput = document.querySelector('#search').value;
 
-//     fetch(`https://api.giphy.com/v1/gifs/translate?api_key=JV9CoVu8Ho9JY70sChP1oVqTM09Z2AJF&s=${searchinput}`)
-//     .then(function(response) {
-//       return response.json();
-//     })
-//     .then(function(data) {
-//       img.src = data.data.images.original.url;
-//     });
+// //     fetch(`https://api.giphy.com/v1/gifs/translate?api_key=JV9CoVu8Ho9JY70sChP1oVqTM09Z2AJF&s=${searchinput}`)
+// //     .then(function(response) {
+// //       return response.json();
+// //     })
+// //     .then(function(data) {
+// //       img.src = data.data.images.original.url;
+// //     });
+// // });
+
+// searchbtn.addEventListener("click", async ()=>{
+//     try{
+//         let searchinput = document.querySelector('#search').value;
+//         let response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=JV9CoVu8Ho9JY70sChP1oVqTM09Z2AJF&s=${searchinput}`);
+//         let data = await response.json();
+//         img.src = data.data.images.original.url;
+//     } 
+//     catch(error){
+//         console.log("Error occured", error);
+//     }
 // });
-
-  
